e6066cbdbdb98fb402313a2202105e9596a82535
Edmunt Pienkowsky <roed@onet.eu>
2023-03-20 08:29:36 +0100

    Olimex boards: use u-boot-olinuxino
    U-Boot modified by Olimex: http://github.com/OLIMEX/u-boot-olinuxino

--- openwrt/openwrt-imagebuilder-22.03.3-sunxi-cortexa7.fix/fix-image-builder.sh
+++ openwrt/openwrt-imagebuilder-22.03.3-sunxi-cortexa7.fix/fix-image-builder.sh
@@ -2,3 +2,4 @@
 # Use modified DT
 #
 cp ${FIXDIR}/sun8i-h3-zeropi.dt* ${OPENWRTDIR}/${IMAGEBUILDERDIR}/build_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/linux-sunxi_cortexa7/linux-5.10.*/arch/arm/boot/dts
+cp ${FIXDIR}/u-boot-sunxi-with-spl.bin ${OPENWRTDIR}/${IMAGEBUILDERDIR}/staging_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/image/olimex_a20-olinuxino-lime2-u-boot-with-spl.bin
---
+++ openwrt/openwrt-imagebuilder-22.03.3-sunxi-cortexa8.fix/fix-image-builder.sh
@@ -0,0 +1,5 @@
+#
+# Use modified DT
+#
+cp ${FIXDIR}/u-boot-sunxi-with-spl.bin ${OPENWRTDIR}/${IMAGEBUILDERDIR}/staging_dir/target-arm_cortex-a8+vfpv3_musl_eabi/image/olimex_a10-olinuxino-lime-u-boot-with-spl.bin
+
